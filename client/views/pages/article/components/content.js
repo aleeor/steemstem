d375540bc294d64603a97b7cb2ebe3c6dbff471d
Benjamin Fuks <address@emal.com>
2018-12-23 23:32:50 +0100

    Display of beneficiaries with posts - beneficiaries are finalized

--- client/views/pages/article/components/content.js
+++ client/views/pages/article/components/content.js
@@ -1,7 +1,33 @@
+// Helpers
 Template.content.helpers({
+   // Get the list of all beneficiaries
+   GetBeneficiaries: function(bnf)
+   {
+     if(bnf)
+     {
+       beneficiary_array = []
+       for (i=0; i < bnf.length; i++)
+         beneficiary_array.push([bnf[i]['account'], bnf[i]['weight']/100])
+       beneficiary_array.sort(function(b,a){ return(a[1]-b[1]);})
+       for (i=0; i < beneficiary_array.length; i++)
+       {
+         if(i<beneficiary_array.length-1)
+           beneficiary_array[i][1]='('+beneficiary_array[i][1].toString()+"%); "
+         else
+           beneficiary_array[i][1]='('+beneficiary_array[i][1].toString()+"%)."
+       }
+       return beneficiary_array
+     }
+   },
 
-})
+  // Function allowing to display a single beneficiary
+  DisplayBeneficiary: function(beneficiary) { return beneficiary[0] },
 
+  // Function allowing to display a share of a single beneficiary
+  DisplayShare: function(beneficiary) { return beneficiary[1] }
+ })
+
+// Event associated to the vote button
 Template.content.events({
     'click  #vote': function (event) {
         event.preventDefault()
