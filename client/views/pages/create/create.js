40e60dac1da0cb614e2e1831b23f19aaebf41319
Benjamin Fuks <fuks@lpthe.jussieu.fr>
2019-03-01 11:15:27 +0100

    Mini-commit: adapating to the way steemconnect now deals with posts

--- client/views/pages/create/create.js
+++ client/views/pages/create/create.js
@@ -298,7 +298,8 @@ Template.create.createProject = function(form)
         ['comment_options',
           {
              author: author, permlink: permlink, max_accepted_payout: '1000000.000 SBD',
-             percent_steem_dollars: percent_steem_dollars, allow_votes: true, allow_curation_rewards: true
+             percent_steem_dollars: percent_steem_dollars, allow_votes: true, allow_curation_rewards: true,
+             extensions: []
           }
         ]
       ];
